1f7a628226ae3979f24cbcf15a9bd7559faee3a0
MahlatsePhetla <phetlamahlatse09@gmail.com>
2025-05-06 10:55:40 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,21 +1,2 @@
 
 
---  Question 1
-
-CREATE TABLE OrderProduct(
-OrderID INT,
-CustomerName VARCHAR(100),
-Product VARCHAR(100)
-);
-
- --  Qustion 2
-
-
- CREATE TABLE OrderDetails(
-orderID INT PRIMARY KEY,
-ProductID INT,
-Quantity INT,
-FOREIGN KEY(OrderID )REFERENCES orders(orderID),
-FOREIGN KEY (ProductID) REFERENCES Products(productID)
-
-);
